e1d8eaf5e311e8e9b2205556218ff0ebf40955d9
orionvi1998@gmail.com <15966899+OrionVi1998@users.noreply.github.com>
2023-11-09 17:13:14 +0100

    -ADD IOmart logo.png -UPDATE navbar.php, style.css

--- navbar.php
+++ navbar.php
@@ -1,9 +1,14 @@
 <div class="navbar">
-  <a href=""> Section A1 </a>
-  <a href=""> Section A2 </a>
+    <img src="./Assets/IOmart%20Logo.png">
+    <div class="sub_navbar">
+        <a href=""> Section A1 </a>
+        <a href=""> Section A2 </a>
+    </div>
 
-  <hr/>
-  <h3>Control Panel</h3>
-  <a href=""> Section B1 </a>
-
+    <hr/>
+    <h3>Control Panel</h3>
+    <div class="sub_navbar">
+        <a href=""> Section B1 </a>
+        <a href=""> Section B2 </a>
+    </div>
 </div>
