bdc247bd8e9f54f648b32569dc88b2bb7bcab3cb
nithy <nithyalakshmi.padmanaban@eleve.isep.fr>
2023-11-30 17:17:00 +0100

    Create StoreBox Component

---
+++ Components/StoreBox.php
@@ -0,0 +1,31 @@
+<div class="window">
+
+</div>
+<div class="Storebox">
+    <label class="Storebox_title"></label>
+    <div class="button_inline_container">
+        <div class="button_store picture">
+
+            <h1>STORE NAME</h1>
+
+            <button type ="button" onclick="display()" id="btnid">
+                <!-- Add id to image -->
+                <img id="image"
+                     src=
+                     "https://media.geeksforgeeks.org/wp-content/uploads/20210915115837/gfg3.png"
+                     alt="GFG image" />
+            </button>
+            <script>
+                function display()
+                {
+                    document.getElementById('image')
+                        .style.display = "block";
+                    document.getElementById('btnID')
+                        .style.display = "none";
+                }
+            </script>
+        </div>
+    </div>
+
+</div>
+
---
+++ Components/storeBox.php
@@ -0,0 +1,11 @@
+<div class="window">
+    <div class="controlbox">
+        <div class="Storebox">
+            <h4>STORE NAME</h4>
+            <div class="button_imageplaceholder">
+                <div class="flex_imageplaceholder"></div>
+            </div>
+            <h3>address</h3>
+        </div>
+    </div>
+</div>
