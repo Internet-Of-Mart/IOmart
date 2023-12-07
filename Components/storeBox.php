9ed4ccca78e5a422c2fff180e349ba9e78e1dce8
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2023-12-07 22:51:56 +0100

    Restore StoreBox Component

--- Components/storeBox.php
+++ Components/storeBox.php
@@ -1,31 +1,11 @@
 <div class="window">
-
-</div>
-<div class="Storebox">
-    <label class="Storebox_title"></label>
-    <div class="button_inline_container">
-        <div class="button_store picture">
-
-            <h1>STORE NAME</h1>
-
-            <button type ="button" onclick="display()" id="btnid">
-                <!-- Add id to image -->
-                <img id="image"
-                     src=
-                     "https://media.geeksforgeeks.org/wp-content/uploads/20210915115837/gfg3.png"
-                     alt="GFG image" />
-            </button>
-            <script>
-                function display()
-                {
-                    document.getElementById('image')
-                        .style.display = "block";
-                    document.getElementById('btnID')
-                        .style.display = "none";
-                }
-            </script>
+    <div class="controlbox">
+        <div class="Storebox">
+            <h4>STORE NAME</h4>
+            <div class="image_placeholder">
+                <div class="flex_image_placeholder"></div>
+            </div>
+            <h3>address</h3>
         </div>
     </div>
-
 </div>
-
