ddfb32e11075730bea2ee14d94bd8ee42258ee3f
nithy <nithyalakshmi.padmanaban@eleve.isep.fr>
2023-12-21 17:17:11 +0100

    Edit registration

--- Views/Registration.php
+++ Views/Registration.php
@@ -2,12 +2,20 @@
 include_once '../wrapper/header.php';
 ?>
 <div class="window_login">
-    <img src="./Assets/IOmart%20Logo.png" alt="">
-    <div class="login_box">
+    <img src="../Assets/IOmart%20Logo.png" alt="">
+    <div class="Registration_box">
         <h2>Registration</h2>
         <form action="" method="post">
 
-            <label for="username">Username:
+            <label for="EmployeeNumber">Employee Number:
+            <input type="text" name="EmployeeNumber" required/>
+            </label>
+
+            <label for="username">Firstname:
+                <input type="text" name="username" required/>
+            </label>
+
+            <label for="username">Lastname:
                 <input type="text" name="username" required/>
             </label>
 
@@ -16,8 +24,33 @@ include_once '../wrapper/header.php';
             </label>
 
             <label for="confirm">Confirm Password:
-                <input type="confirm password" name="confirm password" required/>
+                <input type="password" name="confirm password" required/>
             </label>
+
+            <label for="email">Email:
+                <input type="email" name="email" required/>
+            </label>
+
+            <label for="Telephone">Telephone:
+                <input type="tel" name="telephone" required/>
+            </label>
+
+            <label for="address">Address:
+                <input type="text" name="address" required/>
+            </label>
+
+            <label for="DateOfBirth">DateOfBirth:
+                <input type="date" name="DateOfBirth" required/>
+            </label>
+
+            <label for="EmploymentDate">EmploymentDate:
+                <input type="date" name="username" required/>
+            </label>
+
+
+
+
+
             <button type="submit">Create</button>
         </form>
 
