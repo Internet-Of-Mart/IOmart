ff1a884dda7bb410e5fe628861bbbbd732237e50
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2023-12-21 13:57:14 +0100

    StoreData.php

---
+++ Components/StoreDataTab.php
@@ -0,0 +1,20 @@
+<div class="window">
+    <div class="window_box_container">
+        <div class="window_box">
+            <div class="tab_container">
+                <a href="">Lights</a>
+                <a href="">Temp</a>
+                <a href="">Humidity</a>
+            </div>
+            <div class="graph_window">
+                <?php
+                include_once "../Components/graph_1DS.php"
+                ?>
+            </div>
+            <div class="store_index_container">
+                <div class="color_box"></div>
+                <a>STORENAME</a>
+            </div>
+        </div>
+    </div>
+</div>
--- Views/StoreData.php
+++ Views/StoreData.php
@@ -2,9 +2,11 @@
 include_once '../wrapper/header.php';
 include_once '../wrapper/session_checker.php';
 ?>
-<div class="window_for_tab">
 
-</div>
+<?php
+include '../Components/StoreDataTab.php'
+?>
+
 <?php
 include_once '../wrapper/footer.php'
 ?>
