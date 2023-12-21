1b416b5287c5cf8a3580795827b05687c260343c
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2023-12-21 09:23:24 +0100

    Update SectionManagement.php

--- Views/SectionManagement.php
+++ Views/SectionManagement.php
@@ -2,9 +2,34 @@
 include_once '../wrapper/header.php';
 include_once '../wrapper/session_checker.php';
 ?>
-<div class="window_for_tab">
 
+<div class="window">
+    <div class="window_section_container">
+
+        <?php
+        include_once($_SERVER['DOCUMENT_ROOT'] . '/Model/Section.php');
+
+        use model\Section;
+
+        $elements = [ /*Get data from db instead*/
+            Section::generateDemo1(),
+            Section::generateDemo2(),
+            Section::generateDemo3()
+        ];
+
+        foreach ($elements as $element) {
+            include '../Components/SectionManagementBox.php';
+        }
+
+        ?>
+
+        <?php
+        include '../Components/SectionAddButton.php';
+        ?>
+
+    </div>
 </div>
+
 <?php
 include_once '../wrapper/footer.php'
 ?>
