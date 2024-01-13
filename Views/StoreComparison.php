354fd5fff15e359caa54d42bfbf7a0c16a97b4e2
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-01-13 22:57:38 +0100

    Refactor graph_MDS.php, StoreComparison.php

--- Views/StoreComparison.php
+++ Views/StoreComparison.php
@@ -3,9 +3,33 @@ include_once '../wrapper/header.php';
 include_once '../wrapper/session_checker.php';
 ?>
 
-<?php
-include_once '../Components/StoreComparisonTab.php'
-?>
+<div class="window">
+    <div class="window_box_container">
+        <div class="window_box">
+
+            <?php
+            if ($_GET['tab'] === null) {
+                $path = strtok($_SERVER["REQUEST_URI"], '?') . '?tab=lights';
+                header("location: $path");
+            }
+            ?>
+
+            <div class="tab_container">
+                <a href="<?php
+                echo strtok($_SERVER["REQUEST_URI"], '?') . '?tab=lights' ?>">Lights</a>
+                <a href="<?php
+                echo strtok($_SERVER["REQUEST_URI"], '?') . '?tab=temperature' ?>">Temp</a>
+                <a href="<?php
+                echo strtok($_SERVER["REQUEST_URI"], '?') . '?tab=humidity' ?>">Humidity</a>
+            </div>
+
+            <?php
+            include_once "../Components/graph_MDS.php"
+            ?>
+
+        </div>
+    </div>
+</div>
 
 <?php
 include_once '../wrapper/footer.php'
