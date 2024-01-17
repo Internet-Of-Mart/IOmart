11b212ce886982b455cf5589d3ccd81e6db01a01
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-01-17 15:22:33 +0100

    Fix storeBox.php

--- Components/storeBox.php
+++ Components/storeBox.php
@@ -3,16 +3,13 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/Model/Store.php');
 use model\Store;
 ?>
 <div class="storebox">
-    <div class="outer">
-
-            <h4><?php echo $element->name ?></h4>
-            <div class="image_placeholder">
-                <div class="flex_image_placeholder"></div>
-            </div>
-            <h3><?php echo $element->address ?></h3>
-            <form action="../routing/utilStoreSelect.php" method="post">
-                <input type="hidden" name="store_id" value=<?php echo $element->id?>>
-                <input type="submit" value="Select store" />
-            </form>
+    <h4><?php echo $element->name ?></h4>
+    <div class="image_placeholder">
+        <div class="flex_image_placeholder"></div>
     </div>
+    <h3><?php echo $element->address ?></h3>
+    <form action="../routing/utilStoreSelect.php" method="post">
+        <input type="hidden" name="store_id" value=<?php echo $element->id?>>
+        <input type="submit" value="Select store" />
+    </form>
 </div>
