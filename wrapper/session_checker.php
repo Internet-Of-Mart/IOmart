acbc39da70060c2592d00cf50e9a10ef8019dc0a
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-01-21 14:12:10 +0100

    Reroute the admin back to storeManagement if no store is selected

--- wrapper/session_checker.php
+++ wrapper/session_checker.php
@@ -1,16 +1,28 @@
 <?php
 session_start();
 
+$views = [
+    'admin' => [
+        '/Views/UserManagement.php',
+        '/Views/StoreComparison.php',
+        '/Views/StoreManagement.php',
+    ]
+];
+
 if (!isset($_SESSION['user'])) {
     header('location:../index.php');
 } else {
-
     $user = json_decode($_SESSION['user']);
 
     if ($user->position === 1) {
         if (!$_SESSION['store_id']) {
-            include_once '../navbar_admin.php';
+            $path = strtok($_SERVER["REQUEST_URI"], '?');
 
+            if (!in_array($path, $views['admin'])) {
+                header('location: ../Views/StoreManagement.php');
+            }
+
+            include_once '../navbar_admin.php';
         } else {
             include_once '../navbar.php';
         }
