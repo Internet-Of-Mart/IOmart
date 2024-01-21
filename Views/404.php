88b9eb89bb3b685c7a17ae3d432c72a6d9863610
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-01-21 14:23:42 +0100

    Add other views

---
+++ Views/404.php
@@ -0,0 +1,16 @@
+<?php
+include_once '../wrapper/header.php';
+include_once '../wrapper/session_checker.php';
+?>
+
+<div class="window_login">
+    <img src="./Assets/IOmart%20Logo.png" alt="">
+    <div class="login_box">
+        <h1>404</h1>
+        <h2>Page not found</h2>
+    </div>
+</div>
+
+<?php
+include_once '../wrapper/footer.php'
+?>
--- wrapper/session_checker.php
+++ wrapper/session_checker.php
@@ -6,8 +6,22 @@ $views = [
         '/Views/UserManagement.php',
         '/Views/StoreComparison.php',
         '/Views/StoreManagement.php',
+    ],
+    'notAdmin' => [
+        '/Views/AccountPanel.php',
+        '/Views/Humidity.php',
+        '/Views/Light.php',
+        '/Views/Login.php',
+        '/Views/Registration.php',
+        '/Views/SectionManagement.php',
+        '/Views/StoreData.php',
+        '/Views/Temperature.php',
+        '/Views/404.php',
+        '/index.php',
     ]
 ];
+
+$path = strtok($_SERVER["REQUEST_URI"], '?');
 
 if (!isset($_SESSION['user'])) {
     header('location:../index.php');
@@ -16,7 +30,6 @@ if (!isset($_SESSION['user'])) {
 
     if ($user->position === 1) {
         if (!$_SESSION['store_id']) {
-            $path = strtok($_SERVER["REQUEST_URI"], '?');
 
             if (!in_array($path, $views['admin'])) {
                 header('location: ../Views/StoreManagement.php');
