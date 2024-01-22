9a530ad2e861460a1dc60bd5580d929abdd2279a
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-01-22 10:59:31 +0100

    Refactor

---
+++ Components/EditAccount.php
@@ -0,0 +1,100 @@
+<?php
+/** @var User $account */
+
+/* @var Array $adminStores */
+
+use model\User;
+use model\Store;
+
+?>
+
+<script>
+    function openEditForm<?php echo $account->id?>() {
+        document.getElementById("edit-user-" + <?php echo $account->id?>).style.display = "block";
+        document.getElementById("action-fade-" + <?php echo $account->id?>).style.display = "block";
+    }
+
+    function closeEditForm<?php echo $account->id?>() {
+        document.getElementById("edit-user-" + <?php echo $account->id?>).style.display = "none";
+        document.getElementById("action-fade-" + <?php echo $account->id?>).style.display = "none";
+
+    }
+</script>
+
+<button class="edit_btn" onclick="openEditForm<?php echo $account->id ?>()">
+    <?php include '../Assets/editSvg.php' ?>
+</button>
+
+<div class="store-form-popup" id="<?php echo "edit-user-" . $account->id ?>">
+
+    <h4>Editing User</h4>
+    <form action="../routing/utilEditUser.php" method="post" style="display: flex; flex-direction: column">
+        <input type="hidden" name="user_id" value=<?php echo $account->id ?>>
+
+        <h5>Personal details</h5>
+
+        <div class="fieldPadding15px">
+            <label for="first_name">First Name:
+                <input type="text" name="first_name" value="<?php echo $account->firstname ?>" required/>
+            </label>
+
+            <label for="last_name">Last Name (in Uppercase):
+                <input type="text" name="last_name" value="<?php echo $account->lastname ?>" pattern="[A-Z]+" required/>
+            </label>
+        </div>
+
+        <div class="fieldPadding15px">
+            <label for="date_of_birth">Date of Birth:
+                <input type="date" name="date_of_birth" value="<?php echo $account->dob ?>" required/>
+            </label>
+            <label for="address">Address:
+                <input type="text" name="address" value="<?php echo $account->address ?>" required/>
+            </label>
+            <label for="telephone">Telephone:
+                <input type="tel" name="telephone" value="<?php echo $account->telephone ?>" required/>
+            </label>
+        </div>
+
+        <label for="email">Email:
+            <input type="email" name="email" value="<?php echo $account->email ?>" required/>
+        </label>
+
+
+        <hr style="margin: 20px"/>
+        <h5>Employment details</h5>
+
+        <div class="fieldPadding15px">
+            <label for="employee_number">Employee Number:
+                <input type="text" name="employee_number" value="<?php echo $account->employee_number ?>" required/>
+            </label>
+
+            <label for="date_of_employment">Employment Date:
+                <input type="date" name="date_of_employment" value="<?php echo $account->do_employment ?>" required/>
+            </label>
+        </div>
+
+        <hr style="margin: 20px"/>
+        <h5>Credentials</h5>
+
+        <div class="fieldPadding15px">
+            <label for="username">Username:
+                <input type="text" name="username" required/>
+            </label>
+
+            <label for="password">Password:
+                <input type="password" name="password" required/>
+            </label>
+
+            <label for="confirm">Confirm Password:
+                <input type="password" name="confirm"  required/>
+            </label>
+        </div>
+
+        <div>
+            <button type="submit" class="active_on">Edit User</button>
+            <button type="button" class="edit_btn cancel" onclick="closeEditForm<?php echo $account->id ?>()">Cancel
+            </button>
+        </div>
+    </form>
+</div>
+<div id="<?php echo "action-fade-" . $account->id ?>" class="black-fade"></div>
--- Views/AccountPanel.php
+++ Views/AccountPanel.php
@@ -4,6 +4,7 @@ include_once '../wrapper/session_checker.php';
 
 include $_SERVER['DOCUMENT_ROOT'] . '/Model/User.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/Model/Store.php';
+
 use model\Store;
 use model\User;
 
@@ -11,40 +12,90 @@ $account = User::getSessionUser();
 $cred = User::accountCred($account->id);
 $pos = User::getPosition($account->position);
 $aff = Store::getUserStores($account->id);
+
 ?>
 
-<div class="window">
-    <div class="window_box_container">
-        <div class="account_window">
-            <div class="account_title_container">
-                <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512">
-                    <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                    <style>svg{fill:#000000}</style>
-                    <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
-                <span><h1><?php echo $account->firstname. ' ' .$account->lastname ?> </h1></span>
-            </div>
-            <form class="account_panel">
-                <h3>Personal Data</h3>
-                <h5><label for="email">Email: <?php echo $account->email?></label><br></h5>
-                <h5><label for="telephone">Telephone: <?php echo $account->telephone?></label><br></h5>
-                <h5><label for="address">Address: <?php echo $account->address?></label><br></h5>
-                <h5><label for="date_of_birth">Date Of Birth: <?php echo $account->dob?></label><br></h5>
-                <h3>Employee Data</h3>
-                <h5><label for="privilege">Privilege: <?php echo $pos['name'];?></label><br></h5>
-                <h5><label for="affiliatedStore">Affiliated Store: <br> <?php foreach ($aff as $store) {
-                            echo $store->name . "<br>";
-                        };?></label><br></h5>
-                <h5><label for="employee_number">Employee Number: <?php echo $account->employee_number?> </label><br></h5>
-                <h5><label for="date_of_employment">Employment Date: <?php echo $account->do_employment?> </label><br></h5>
-                <h3>Credentials</h3>
-                <h5><label for="username">Username: <?php echo $cred['username']?></label><br></h5>
+    <div class="window">
+        <div class="window_box_container">
+            <div class="account_window">
+                <div class="account_title_container">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512">
+                        <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <style>svg {
+                                fill: #000000
+                            }</style>
+                        <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
+                    </svg>
+                    <h1><?php echo $account->firstname . ' ' . $account->lastname ?> </h1>
+                </div>
+
+                <div>
+                    <h3>Personal Data</h3>
+                    <h5>
+                        <label for="email">Email: <?php echo $account->email ?>
+                        </label><br>
+                    </h5>
+                    <h5>
+                        <label for="telephone">Telephone: <?php echo $account->telephone ?>
+                        </label><br>
+                    </h5>
+                    <h5>
+                        <label for="address">Address: <?php echo $account->address ?>
+                        </label><br>
+                    </h5>
+                    <h5>
+                        <label for="date_of_birth">Date Of Birth:
+                            <?php echo $account->dob ?>
+                        </label><br>
+                    </h5>
+                </div>
+
+
+                <div>
+                    <h3>Employee Data</h3>
+                    <h5>
+                        <label for="privilege">Privilege: <?php echo $pos['name']; ?>
+                        </label><br>
+                    </h5>
+                    <h5>
+                        <label for="affiliatedStore">Affiliated Stores: <br>
+                            <?php foreach ($aff as $store) {
+                                echo "- " . $store->name . "<br>";
+                            } ?>
+                        </label>
+                        <br>
+                    </h5>
+
+                    <h5><label for="employee_number">Employee
+                            Number: <?php echo $account->employee_number ?>
+                        </label><br>
+                    </h5>
+                    <h5>
+                        <label for="date_of_employment">Employment
+                            Date: <?php echo $account->do_employment ?>
+                        </label><br>
+                    </h5>
+                </div>
+
+
+
+                <div>
+                    <h3>Credentials</h3>
+                    <h5>
+                        <label for="username">Username:
+                            <?php echo $cred['username'] ?>
+                        </label>
+                        <br>
+                    </h5>
+                </div>
+
                 <?php
                 include '../Components/EditAccount.php'
                 ?>
-            </form>
+            </div>
         </div>
     </div>
-</div>
+
 <?php
 include_once '../wrapper/footer.php'
 ?>
